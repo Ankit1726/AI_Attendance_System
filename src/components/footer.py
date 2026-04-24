134cc9c0375d694f9618e253125cb8d3752fc570
Ankit Gupta <165042198+Ankit1726@users.noreply.github.com>
2026-04-24 13:07:29 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -4,7 +4,7 @@ import streamlit as st
 def footer_home():
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with Ankit Gupta by ❤️
+        <p style="font-weight:bold; color:white;"> Created with Team by ❤️
         </div>
                 
                 """, unsafe_allow_html=True)
@@ -15,7 +15,7 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with Ankit Gupta by ❤️ </p>  
+        <p style="font-weight:bold; color:white;"> Created with Team by ❤️ </p>  
         </div>
                 
                 """, unsafe_allow_html=True)
